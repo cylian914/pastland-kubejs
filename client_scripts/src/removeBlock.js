4e0aa4e5836d0160dc8a2d751eb0ea4334001d13
cylian charbonnier <cylian.charbonnier1@gmail.com>
2025-12-17 04:22:50 +0100

    indent correctly

--- client_scripts/src/removeBlock.js
+++ client_scripts/src/removeBlock.js
@@ -1,8 +1,9 @@
 JEIEvents.hideItems(e => {
-	global.get("OBWGwood").forEach((w) => {
-		global.get("drawerType").forEach((d) => {
-			e.hide("storagedrawersextra:biomeswevegone_" + w + "_" + d)
-		})
+    global.get("OBWGwood").forEach((w) => {
+	global.get("drawerType").forEach((d) => {
+	    e.hide("storagedrawersextra:biomeswevegone_" + w + "_" + d)
 	})
-
+    })
 })
+
+
