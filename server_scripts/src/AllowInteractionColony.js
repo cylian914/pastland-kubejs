175fe6f22b40a26de6371c3767c365f9d8b6a3a8
cylian charbonnier <cylian.charbonnier1@gmail.com>
2025-12-17 04:23:48 +0100

    Allow corpse/imersiveAirplane interaction colony

---
+++ server_scripts/src/AllowInteractionColony.js
@@ -0,0 +1,11 @@
+ServerEvents.tags("entity_types", (e) => {
+    e.add('minecolonies:allowinteract', [
+        'immersive_aircraft:airship',
+        'immersive_aircraft:bomboo_hopper',
+        'immersive_aircraft:biplane',
+        'immersive_aircraft:cargo_airship',
+        'immersive_aircraft:gyrodyne',
+        'immersive_aircraft:quadrocopter',
+        'immersive_aircraft:warship'
+    ])
+})
