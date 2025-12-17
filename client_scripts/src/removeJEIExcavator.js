2c5f6e6583e40b14551a2240fdb902f80eb15210
cylian charbonnier <cylian.charbonnier1@gmail.com>
2025-12-17 04:22:09 +0100

    remove more of excavator

---
+++ client_scripts/src/removeJEIExcavator.js
@@ -0,0 +1,6 @@
+JEIEvents.hideItems(e => {
+    e.hide("immersiveengineering:excavator");
+    e.hide("immersiveengineering:bucket_wheel");
+    e.hide("immersiveengineering:sample_drill");
+    e.hide("immersiveengineering:coresample");
+})
--- server_scripts/src/Disable_Excavators.js
+++ server_scripts/src/Disable_Excavators.js
@@ -2,11 +2,8 @@
 ServerEvents.recipes((event) => {
     //removing vein result in a crash due to IE villager trying to find a vein
     //TODO find a solution
-    /*event.recipeStream({type: "immersiveengineering:mineral_mix"}).forEach((recipe) => {
-	if (recipe.getId() == "immersiveengineering:mineral/igneous_rock")
-	    return; //let 1 non harmfull one to avoid villager crash
-	recipe.remove();
-    });*/
-    event.remove({type: "immersiveengineering:mineral_mix"});
+
+
     event.remove({id: "immersiveengineering:crafting/survey_tools"});
+    event.remove({id: "immersiveengineering:crafting/sample_drill"})
 });
